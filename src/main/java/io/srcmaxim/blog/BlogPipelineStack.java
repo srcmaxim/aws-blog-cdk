cf13919911dca67224a1b565375da9257e1dc27e
maksym.koval <srcmaxim@gmail.com>
2020-11-19 23:24:22 +0200

    Update CDK deployment CFN access

--- src/main/java/io/srcmaxim/blog/BlogPipelineStack.java
+++ src/main/java/io/srcmaxim/blog/BlogPipelineStack.java
@@ -82,6 +82,7 @@ public class BlogPipelineStack extends Stack {
         var cfnDescribeStacks = PolicyStatement.Builder.create()
                 .actions(List.of(
                         "cloudformation:DescribeStacks",
+                        "cloudformation:CreateChangeSet",
                         "s3:*"
                 ))
                 .resources(List.of("*"))
